cf0900c22952a30afb6c65c1611fb97476ccef2f
devUserContact <developer.user.contact@protonmail.com>
2022-08-27 15:36:32 -0700

    edited controller

---
+++ src/main/java/com/api/bluehappyface/Controller/Controllers.java
@@ -0,0 +1,14 @@
+package com.api.bluehappyface.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controllers {
+
+	@GetMapping(value = "/")
+	public String getPage() {
+		return "bluehappyface api";
+	}
+
+}
