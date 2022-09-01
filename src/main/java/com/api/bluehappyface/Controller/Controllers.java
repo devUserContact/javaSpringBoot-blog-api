4c94880b7059903b5c2723b5383391b77561e0b6
devUserContact <developer.user.contact@protonmail.com>
2022-09-01 12:23:28 -0700

    cors is functional

--- src/main/java/com/api/bluehappyface/Controller/Controllers.java
+++ src/main/java/com/api/bluehappyface/Controller/Controllers.java
@@ -1,9 +1,11 @@
 package com.api.bluehappyface.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class Controllers {
 
 	@GetMapping(value = "/")
--- src/main/java/com/api/bluehappyface/CorsConfig/CorsConfig.java
+++ src/main/java/com/api/bluehappyface/CorsConfig/CorsConfig.java
@@ -1,20 +1,26 @@
 package com.api.bluehappyface.CorsConfig;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http:/localhost:8081")
-            .allowedMethods("*")
-            .allowedHeaders("*")
-            .allowCredentials(false)
-            .maxAge(3600);
-    }
+public class CorsConfig {
+ 
+	@Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+ 
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+    corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return new CorsFilter(source);
+  }
 }
